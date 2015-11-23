2f8c5f3a19276da8cc9a76e2ecc139232b6e1b63
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-11-23 18:34:28 +0000

    use paging repo for Mongo

--- src/main/java/uk/gov/nationalarchives/ttt/neo4j/dao/mongo/PersonDocumentRepository.java
+++ src/main/java/uk/gov/nationalarchives/ttt/neo4j/dao/mongo/PersonDocumentRepository.java
@@ -1,12 +1,12 @@
 package uk.gov.nationalarchives.ttt.neo4j.dao.mongo;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import uk.gov.nationalarchives.ttt.neo4j.domain.graphperson.MongoPerson;
 
 /**
  * Created by jcharlet on 14/11/15.
  */
 public interface PersonDocumentRepository
-        extends MongoRepository<MongoPerson, String>, PersonDocumentRepositoryCustom
+        extends PagingAndSortingRepository<MongoPerson, String>, PersonDocumentRepositoryCustom
 {
 }
--- src/test/java/uk/gov/nationalarchives/ttt/neo4j/TttNeo4jApplicationTests.java
+++ src/test/java/uk/gov/nationalarchives/ttt/neo4j/TttNeo4jApplicationTests.java
@@ -1,16 +1,20 @@
 package uk.gov.nationalarchives.ttt.neo4j;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.neo4j.ogm.session.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import uk.gov.nationalarchives.ttt.neo4j.dao.mongo.PersonDocumentRepository;
 import uk.gov.nationalarchives.ttt.neo4j.dao.neo4j.PersonGraphRepository;
+import uk.gov.nationalarchives.ttt.neo4j.domain.graphperson.MongoPerson;
 import uk.gov.nationalarchives.ttt.neo4j.domain.graphperson.generated.*;
 
 import java.util.ArrayList;
@@ -31,6 +35,7 @@ public class TttNeo4jApplicationTests {
 
     @Autowired
     PersonDocumentRepository personDocumentRepository;
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     private void emptyDatabase() {
         session.query("MATCH (n) DETACH DELETE n", new HashMap<String,Object>());
@@ -40,11 +45,11 @@ public class TttNeo4jApplicationTests {
     public void testSavePeopleFromMongoIntoNeo4j() {
         emptyDatabase();
 
-        List<Person> people = new ArrayList<>();
+        List<MongoPerson> people = new ArrayList<>();
         personDocumentRepository.setPersonCollectionName("WO_98_Discovery_A");
-        people.addAll(personDocumentRepository.findAll());
+        people.addAll(IteratorUtils.toList(personDocumentRepository.findAll().iterator()));
         personDocumentRepository.setPersonCollectionName("WO_98_Discovery_B");
-        people.addAll(personDocumentRepository.findAll());
+        people.addAll(IteratorUtils.toList(personDocumentRepository.findAll().iterator()));
         Assert.assertEquals(4, people.size());
 
         for (Person person:people){
@@ -56,6 +61,28 @@ public class TttNeo4jApplicationTests {
         Assert.assertEquals(4, session.query("MATCH (n:ForeName) RETURN count(n)", new HashMap<>()).queryResults().iterator().next().get("count(n)"));
         Assert.assertEquals(2, session.query("MATCH (:Person) --> (n:FamilyName) <-- (Person) RETURN count(n)", new HashMap<>()).queryResults().iterator().next().get("count(n)"));
         Assert.assertEquals(1, session.query("MATCH (a:ForeName) <-- (n:Person) --> (b:ForeName) RETURN count(DISTINCT n)", new HashMap<>()).queryResults().iterator().next().get("count(DISTINCT n)"));
+    }
+
+//    @Test
+    public void testSavePeopleFromHugeCollectionIntoNeo4j() {
+        emptyDatabase();
+
+        logger.info("Start");
+
+        final int MAX_ELEMENTS=1000;
+        personDocumentRepository.setPersonCollectionName("ADM337_Discovery_eval");
+
+        int counter=0;
+        for (MongoPerson person : personDocumentRepository.findAll()) {
+            personGraphRepository.createOrMergePersonGraph(person);
+            counter++;
+            if(counter>=MAX_ELEMENTS){
+                break;
+            }
+        }
+        logger.info("End");
+
+        Assert.assertEquals(MAX_ELEMENTS, session.query("MATCH (n:Person) RETURN count(n)", new HashMap<>()).queryResults().iterator().next().get("count(n)"));
     }
 
     @Test
